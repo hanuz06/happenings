31e337e7400b30383bd1c28c1de85ed1cc87ae25
Andrey <lee1975uz@gmail.com>
2022-06-14 23:12:47 +0900

    20

--- components/Showcase.js
+++ components/Showcase.js
@@ -3,7 +3,7 @@ import styles from "@/styles/Showcase.module.css";
 export default function Showcase() {
   return (
     <div className={styles.showcase}>
-      <h1>Welcome to the party!</h1>
+      <h1>Life's beautiful with Festivals!</h1>
       <h2>Find the coolest events</h2>
     </div>
   );
