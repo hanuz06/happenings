abfd5361b884c4dd0003fc32ca3deb14e5992fb1
Andrey <lee1975uz@gmail.com>
2022-06-14 23:04:06 +0900

    19

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ import styles from "@/styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; Our Events {new Date().getFullYear()}</p>
+      <p>Copyright &copy; Upcoming Events {new Date().getFullYear()}</p>
       <p>
-        <Link href='/about'>About This Program</Link>
+        <Link href='/about'>About Upcoming Events</Link>
       </p>
     </footer>
   );
--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <Layout title="About our latest events">
       <h1>About</h1>
-      <p>This is an app to find the latest singer</p>
+      <p>This is an app to find the latest festivals and other events</p>
       <p>Version: 1.0.0</p>
       <Link href='/'>Home</Link>
     </Layout>
